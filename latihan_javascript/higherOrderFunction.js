5981adac0d63508845c9db3b59380c72c373317a
ina abdul shukor <ishukor51@gmail.com>
2021-06-12 18:19:17 +0800

    higher order function

--- latihan_javascript/higherOrderFunction.js
+++ latihan_javascript/higherOrderFunction.js
@@ -1,8 +1,10 @@
 
-//ni kita panggil higher order function yang ada function dalam parameter
+//ni kita panggil higher order function yang ada function dalam parameter / return function
 //function pada parameter dipanggil callback
 
 function KerjakanTugas(matakuliah, selesai){
     console.log(`Mulai kerjakan tugas ${matakuliah}...`);
     selesai();
 }
+
+//kenapa? akan membuat program makin simple
