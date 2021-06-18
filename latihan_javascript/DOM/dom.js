651d59e2246e04729dd48e141dbc21d802dd47a7
ina abdul shukor <ishukor51@gmail.com>
2021-06-18 15:05:48 +0800

    dom selection

---
+++ latihan_javascript/DOM/dom.js
@@ -0,0 +1,10 @@
+//represent element html dalam bentuk object
+//heriarcy
+//-window
+//--document
+//--history
+
+//manipulate element html
+//type of node
+//-element , attribute , text , CData Section , Entity Reference , Entity , Processing Instruction , Commment , Document , Document type , Document fragmnet , notation
+ //HTML collection (html ja)- bersifat live vs NodeList (apa2 node) - tidak
